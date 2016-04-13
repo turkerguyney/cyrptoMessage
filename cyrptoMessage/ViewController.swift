8195d3c49520264536d2863db17fea4c622160bc
turker bilge <turker@mehmet-imac.local>
2016-04-13 16:39:12 +0300

    resim uretildi

--- cyrptoMessage/ViewController.swift
+++ cyrptoMessage/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     
     
+    var qrCodeImage:CIImage!
     
     
     @IBOutlet weak var textField: UITextField!
@@ -39,6 +40,83 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func genBtnAct(sender: AnyObject) {
+        
+        if(qrCodeImage == nil){
+        
+        
+            if(textField.text == ""){
+            
+            
+            return
+            
+            
+            }
+            
+           let data = textField.text!.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
+            
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            
+            filter!.setValue(data, forKey: "inputMessage")
+            filter!.setValue("Q", forKey: "inputCorrectionLevel")
+            
+            qrCodeImage = filter!.outputImage
+            
+           
+            displayQRCodeImage()
+            
+            textField.resignFirstResponder()
+            
+            
+            
+            
+            genButton.setTitle("Clear", forState: UIControlState.Normal)
+           
+            
+        
+        
+        }else{
+        
+            imView.image = nil
+            qrCodeImage = nil
+            genButton.setTitle("Generate", forState: UIControlState.Normal)
+            
+        
+        
+        
+        }
+    
+        textField.enabled = !textField.enabled
+        slider.hidden = !slider.hidden
+        
+    }
+
+
+    @IBAction func imageResizerAct(sender: AnyObject) {
+        
+        imView.transform = CGAffineTransformMakeScale(CGFloat(slider.value), CGFloat(slider.value))
+    }
+
+
+    func displayQRCodeImage() {
+        let scaleX = imView.frame.size.width / qrCodeImage.extent.size.width
+        let scaleY = imView.frame.size.height / qrCodeImage.extent.size.height
+        
+        let transformedImage = qrCodeImage.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
+        
+        imView.image = UIImage(CIImage: transformedImage)
+        
+        
+    }
+    
+    
+    
+    
+    
+
 
 }
+
+
 
