b3683b171e6a7d22f0bfb8f5503ffb825556b057
turker bilge <turker@mehmet-imac.local>
2016-04-14 00:40:27 +0300

    social completed

--- cyrptoMessage/ViewController.swift
+++ cyrptoMessage/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Social
+import AssetsLibrary
+import Photos
 
 class ViewController: UIViewController {
     
@@ -14,6 +17,7 @@ class ViewController: UIViewController {
     
     var qrCodeImage:CIImage!
     
+    let metaList: [String:String] = ["Developer": "Turker Bilge Guney"]
     
     @IBOutlet weak var textField: UITextField!
     
@@ -28,10 +32,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     
     
+ 
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -40,6 +46,75 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func saveAct(sender: AnyObject) {
+        
+  
+
+        
+       
+    }
+    
+    
+    
+    @IBAction func twitButtonAct(sender: AnyObject) {
+        
+         /// performSegueWithIdentifier("selectViewSeq", sender:nil)
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            
+            let TwitSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            
+            TwitSheet.setInitialText("Qr Code")
+            TwitSheet.addImage(imView.image)
+            
+            self.presentViewController(TwitSheet, animated: true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Accounts", message: "Please log into Twitter in your settings", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            
+        }
+        
+        
+        
+        
+    }
+    @IBAction func faceButtonAct(sender: AnyObject) {
+        
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            
+            let FacebookSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            
+           
+            FacebookSheet.addImage(imView.image)
+            
+            self.presentViewController(FacebookSheet, animated: true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Accounts", message: "Please log into facebook in your settings", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
     @IBAction func genBtnAct(sender: AnyObject) {
         
         if(qrCodeImage == nil){
@@ -53,6 +128,10 @@ class ViewController: UIViewController {
             
             }
             
+            textField.resignFirstResponder()
+            
+            
+            
            let data = textField.text!.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
             
             
@@ -63,10 +142,15 @@ class ViewController: UIViewController {
             
             qrCodeImage = filter!.outputImage
             
-           
+            
+            
+            
+            
+            
+            
             displayQRCodeImage()
             
-            textField.resignFirstResponder()
+            
             
             
             
@@ -105,11 +189,38 @@ class ViewController: UIViewController {
         
         let transformedImage = qrCodeImage.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
         
-        imView.image = UIImage(CIImage: transformedImage)
         
         
+        
+        let softwareContext = CIContext(options: [kCIContextUseSoftwareRenderer:true])
+        let cgimg = softwareContext.createCGImage(transformedImage, fromRect: transformedImage.extent)
+        
+      
+        
+        
+        imView.image = UIImage(CGImage: cgimg)
+        
+        
+        
+        
+
+        
+        
+       
     }
     
+    
+    func saveImage(image: UIImage, path: String) -> Bool {
+        let pngImageData = UIImagePNGRepresentation(image)
+        let result = pngImageData!.writeToFile(path, atomically: true)
+        return result
+    }
+    
+    func loadImageFromPath(path: String) -> UIImage? {
+        let data = NSData(contentsOfFile: path)
+        let image = UIImage(data: data!)
+        return image
+    }
     
     
     
